a9044d7e94eabb9d767e627f701134289c0272cb
Solar Sailor <solarsailorneo@protonmail.com>
2023-02-26 03:47:42 +0000

    trl running with karpathy's

---
+++ __init__.py
@@ -0,0 +1,11 @@
+# flake8: noqa
+
+__version__ = "0.2.1"
+
+from .models import (
+    AutoModelForCausalLMWithValueHead,
+    AutoModelForSeq2SeqLMWithValueHead,
+    PreTrainedModelWrapper,
+    create_reference_model,
+)
+from .trainer import PPOConfig, PPOTrainer
